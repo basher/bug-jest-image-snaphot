2b2976b9935346b063e2a58ffc2f3759262b9a39
basher <basher@madasafish.com>
2018-10-01 08:34:40 +0100

    Added { toMatchImageSnapshot } curly braces!

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,6 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import toMatchImageSnapshot from 'jest-image-snapshot';
+import { toMatchImageSnapshot } from 'jest-image-snapshot';
 
 configure({ adapter: new Adapter() });
 
